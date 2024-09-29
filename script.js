321fd68d93dcfe6ac199b9d6f160f7afe7d5f596
bigpimplimp <levi.jcecil@gmail.com>
2024-09-29 16:53:15 -0700

    fix add book objects to array

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     // this.read = read;
 }
 
-// const bookName = '';
-// const authorName = '';
-// const numPages = '';
+let bookName;
+let authorName;
+let numPages;
+let newBook; 
+
+document.getElementById('addBook')
+.addEventListener('click', (e) => {
+    bookName = document.getElementById('book_name').value;
+    authorName = document.getElementById('author_name').value;
+    numPages = document.getElementById('num_pages').value;
+    newBook = new Book(bookName, authorName, numPages);
+    myLibrary.push(newBook);
+    e.preventDefault();
+});
 
 
-function addBookToLibrary(form) {
-   document.getElementById('addBook')
-   .addEventListener('click', (e) => {
-    const bookName = document.getElementById('book_name').value;
-    alert(bookName);
-    const authorName = document.getElementById('author_name').value;
-    alert(authorName);
-    const numPages = document.getElementById('num_pages').value;
-    alert(numPages);
-    const newBook = new Book(bookName, authorName, numPages);
-    console.log(newBook);
-    // myLibrary.push(newBook);
-   })
+
+let main = document.getElementById('main');
+
+function addBookToDisplay(arr) {
+    arr.forEach(element => {
+        let myString = JSON.stringify(element);
+        // const div1 = document.createElement('div');
+        let paragraph1 = document.createElement('div')
+        .innerHTML = myString;
+        document.body.appendChild(paragraph1);
+    });
+}
+
+function addBookToDisplay2(arr) {
 
 }
 
-addBookToLibrary();
 
-function addBookToDisplay() {
 
-}
+
+
+// myLibrary.push(newBook1);
+// addBookToDisplay(myLibrary);
